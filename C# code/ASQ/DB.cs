7c219fc73142710ae366fca1bfeed3dae7caaf44
Maksim Zelenin <maksim.zelenin.01@bk.ru>
2021-06-11 22:04:54 +0500

    Установил соединение с удаленной бд

---
+++ C# code/ASQ/DB.cs
@@ -0,0 +1,29 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASQ
+{
+    class DB
+    {
+        MySqlConnection connection = new MySqlConnection("Database=ch88635_asq;Data Source=185.114.247.102;User Id=ch88635_asq;Password=ASQpassBD1");
+        public void openConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+                connection.Open();
+        }
+        public void closeConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+                connection.Close();
+        }
+
+        public MySqlConnection GetConnection()
+        {
+            return connection;
+        }
+    }
+}
